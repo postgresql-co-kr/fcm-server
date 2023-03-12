b56cf881d022b330030c6bec9490307fed826c89
jyo <jinyoonoh@gmail.com>
2023-03-12 22:00:24 +0900

    add timed aspect

--- src/main/java/com/ecobridge/fcm/server/controller/FcmApiController.java
+++ src/main/java/com/ecobridge/fcm/server/controller/FcmApiController.java
@@ -1,6 +1,7 @@
 package com.ecobridge.fcm.server.controller;
 
 import com.ecobridge.fcm.server.service.FcmApiService;
+import io.micrometer.core.annotation.Timed;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class FcmApiController {
     }
 
     @RequestMapping("/hello")
+    @Timed(value = "hello.time", description = "hello response time")
     public String hello() {
         return "hello fcm";
     }
