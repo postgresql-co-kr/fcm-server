01d9b0f317548f8c00a12e2b151216a3e263e766
Jyo <dragonball365@gmail.com>
2023-04-02 02:20:27 +0900

    add cache, redis

--- src/main/java/com/ecobridge/fcm/common/repository/UserRolesEntityRepository.java
+++ src/main/java/com/ecobridge/fcm/common/repository/UserRolesEntityRepository.java
@@ -2,10 +2,11 @@ package com.ecobridge.fcm.common.repository;
 
 import com.ecobridge.fcm.common.entity.UserRolesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface UserRolesEntityRepository extends JpaRepository<UserRolesEntity, Long> {
     Optional<List<UserRolesEntity>> findByUserId(Long userId);
 
--- src/main/java/com/ecobridge/fcm/common/repository/UsersEntityRepository.java
+++ src/main/java/com/ecobridge/fcm/common/repository/UsersEntityRepository.java
@@ -2,9 +2,11 @@ package com.ecobridge.fcm.common.repository;
 
 import com.ecobridge.fcm.common.entity.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UsersEntityRepository extends JpaRepository<UsersEntity, Long> {
     boolean existsByUsername(String username);
     Optional<UsersEntity> findByUsername(String username);
